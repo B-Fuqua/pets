6a5a9afda9712fc477bcada22a95368feb215897
Ben Fuqua <bfuqua@mail.greenriver.edu>
2021-04-27 13:56:34 -0400

    added index page

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload file
+require_once ('vendor/autoload.php');
+
+//Instantiate Fat-Free
+$f3 = Base::instance();
+
+//Define routes
+$f3->route('GET /', function(){
+    echo "<h1>Pet Home</h1>";
+});
